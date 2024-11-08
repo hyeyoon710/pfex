1d06a950a5736166011547638e8429bb53fa3516
HY <coco5303@naver.com>
2024-11-08 17:09:11 +0900

    commit

--- JS/main.js
+++ JS/main.js
@@ -1,33 +1,71 @@
 const box = document.querySelector('.p_wrap');
 console.log(box.offsetWidth, box.offsetHeight);
 
+// let lastScrollTop = 0; // 이전 스크롤 위치 저장
+
+// $(window).on('scroll', function() {
+//   let currentScrollTop = $(this).scrollTop(); // 현재 스크롤 위치
+
+//   if (currentScrollTop > 110) {
+
+//     if (currentScrollTop > lastScrollTop) {
+//       // 아래로 스크롤 중이면 헤더 숨기기
+//       $('header').removeClass('fixed').slideUp();
+//     } else {
+//       // 위로 스크롤 중이면 헤더 보이기
+//       $('header').addClass('fixed').slideDown();
+//     }
+//   } else {
+//     // 스크롤이 110px 이하일 때는 헤더가 보임
+//     $('header').removeClass('fixed').slideDown();
+//   }
+//   lastScrollTop = currentScrollTop; // 이전 스크롤 위치 업데이트
+// });
+
+// // section move
+// $('header a').click(function(e) {
+//   e.preventDefault()
+
+//   if ($(this).hasClass('home')) {
+//     window.location.href = 'index.html'
+//     return; // 아래 섹션 이동 코드는 실행하지 않음
+//   }
+
+//   let n = $(this).parent().index(); // 클릭한 a 태그의 부모 li의 인덱스를 가져옴
+//   $('html, body').animate({
+//     scrollTop: $('section').eq(n + 1).offset().top
+//   }, 500);
+// })
 let lastScrollTop = 0; // 이전 스크롤 위치 저장
+const portSectionTop = $('.port').offset().top; // .port 섹션의 상단 위치를 계산
 
 $(window).on('scroll', function() {
   let currentScrollTop = $(this).scrollTop(); // 현재 스크롤 위치
 
   if (currentScrollTop > 110) {
-
     if (currentScrollTop > lastScrollTop) {
       // 아래로 스크롤 중이면 헤더 숨기기
       $('header').removeClass('fixed').slideUp();
     } else {
-      // 위로 스크롤 중이면 헤더 보이기
-      $('header').addClass('fixed').slideDown();
+      // 위로 스크롤 중이고 화면 너비가 500px 초과거나, .port 섹션 위로 스크롤 했을 때만 헤더 보이기
+      if (window.innerWidth > 500 || currentScrollTop < portSectionTop) {
+        $('header').addClass('fixed').slideDown();
+      }
     }
   } else {
     // 스크롤이 110px 이하일 때는 헤더가 보임
     $('header').removeClass('fixed').slideDown();
   }
+
   lastScrollTop = currentScrollTop; // 이전 스크롤 위치 업데이트
 });
 
 // section move
 $('header a').click(function(e) {
-  e.preventDefault()
+  e.preventDefault();
 
   if ($(this).hasClass('home')) {
-    window.location.href = 'index.html'
+    window.location.href = 'index.html';
     return; // 아래 섹션 이동 코드는 실행하지 않음
   }
 
@@ -35,8 +73,7 @@ $('header a').click(function(e) {
   $('html, body').animate({
     scrollTop: $('section').eq(n + 1).offset().top
   }, 500);
-})
-
+});
 // ScrollTrigger
 window.addEventListener('load', () => {
   gsap.registerPlugin(ScrollTrigger);
