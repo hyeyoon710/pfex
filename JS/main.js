f2cabf2d87cbfbdd501f1e54e77cafec3528e5c0
HY <coco5303@naver.com>
2024-11-11 11:20:50 +0900

    commit

--- JS/main.js
+++ JS/main.js
@@ -1,46 +1,11 @@
 const box = document.querySelector('.p_wrap');
 console.log(box.offsetWidth, box.offsetHeight);
 
-// let lastScrollTop = 0; // 이전 스크롤 위치 저장
-
-// $(window).on('scroll', function() {
-//   let currentScrollTop = $(this).scrollTop(); // 현재 스크롤 위치
-
-//   if (currentScrollTop > 110) {
-
-//     if (currentScrollTop > lastScrollTop) {
-//       // 아래로 스크롤 중이면 헤더 숨기기
-//       $('header').removeClass('fixed').slideUp();
-//     } else {
-//       // 위로 스크롤 중이면 헤더 보이기
-//       $('header').addClass('fixed').slideDown();
-//     }
-//   } else {
-//     // 스크롤이 110px 이하일 때는 헤더가 보임
-//     $('header').removeClass('fixed').slideDown();
-//   }
-//   lastScrollTop = currentScrollTop; // 이전 스크롤 위치 업데이트
-// });
-
-// // section move
-// $('header a').click(function(e) {
-//   e.preventDefault()
-
-//   if ($(this).hasClass('home')) {
-//     window.location.href = 'index.html'
-//     return; // 아래 섹션 이동 코드는 실행하지 않음
-//   }
-
-//   let n = $(this).parent().index(); // 클릭한 a 태그의 부모 li의 인덱스를 가져옴
-//   $('html, body').animate({
-//     scrollTop: $('section').eq(n + 1).offset().top
-//   }, 500);
-// })
-let lastScrollTop = 0; // 이전 스크롤 위치 저장
-const portSectionTop = $('.port').offset().top; // .port 섹션의 상단 위치를 계산
+let lastScrollTop = 0;
+const portSectionTop = $('.port').offset().top;
 
 $(window).on('scroll', function() {
-  let currentScrollTop = $(this).scrollTop(); // 현재 스크롤 위치
+  let currentScrollTop = $(this).scrollTop();
 
   if (currentScrollTop > 110) {
     if (currentScrollTop > lastScrollTop) {
@@ -57,7 +22,7 @@ $(window).on('scroll', function() {
     $('header').removeClass('fixed').slideDown();
   }
 
-  lastScrollTop = currentScrollTop; // 이전 스크롤 위치 업데이트
+  lastScrollTop = currentScrollTop;
 });
 
 // section move
@@ -74,6 +39,7 @@ $('header a').click(function(e) {
     scrollTop: $('section').eq(n + 1).offset().top
   }, 500);
 });
+
 // ScrollTrigger
 window.addEventListener('load', () => {
   gsap.registerPlugin(ScrollTrigger);
@@ -85,8 +51,7 @@ window.addEventListener('load', () => {
 
   gsap.to('.rest .front', {
     scale: 45,
-    // duration: 5,
-    transformOrigin: 'center center', // 중심점 기준으로 확대
+    transformOrigin: 'center center',
     scrollTrigger: {
       trigger: '.rest',
       start: 'top top',
@@ -96,9 +61,20 @@ window.addEventListener('load', () => {
       // markers: true,
     }
   });
+  gsap.to('.rest p', {
+    opacity : 1,
+    duration: 1,
+    scrollTrigger: {
+      trigger: '.rest',
+      start: 'top top',
+      end: 'bottom top',
+      scrub: true,
+    }
+  });
   ani1.play();
 })
 
+const buttons = [pb1, pb2, pb3];
 const kaist = document.querySelector('.kaist');
 const pb1 = document.querySelector('.pb_1');
 const posco = document.querySelector('.posco');
@@ -106,7 +82,6 @@ const pb2 = document.querySelector('.pb_2');
 const JavaScript = document.querySelector('.javascript');
 const pb3 = document.querySelector('.pb_3');
 const header = document.querySelector('.pb_3');
-const buttons = [pb1, pb2, pb3]; // 버튼들을 배열로 관리
 
 // 버튼 스타일 초기화 함수
 function resetButtonStyles() {
